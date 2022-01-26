2ff10d136b33b48776b0bc1e97e16097e2f6695b
Maxim Skvortsov <maxim.skvortsov@kodep.ru>
2022-01-26 23:57:59 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:sonarjs/recommended', 'prettier'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+  plugins: ['@typescript-eslint', 'sonarjs', 'prettier'],
+}
